df4f38901b6055f13c2eae3ba33678415eb5d7cf
Andrew Shark <ashark@linuxcomp.ru>
2019-01-15 15:27:40 +0300

    Avoid using hardcoded versions to stop flooding pkgbuild diffs in aur

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-python gen-PKGBUILD.py > PKGBUILD
+if [[ `pacman -Q python-debian 2>&1` == "error: package 'python-debian' was not found" ]]; then
+    echo "You need to install python-debian package"
+    exit 1
+else
+    python gen-PKGBUILD.py > PKGBUILD
+fi
