8629a3f92601b54d0d9a713bcc4b56e87094e131
mesmer <ldepaulaf@gmail.com>
2022-12-21 23:55:19 -0300

    update to 22.40

--- packages_map.py
+++ packages_map.py
@@ -1,33 +1,34 @@
 # Generated with ./gen_packages_map.sh > packages_map.py
-# for driver version 22.20-1462318~22.04
+# for driver version 22.40-1518373.22.04
 
 packages_map = {
-    'amdgpu-pro':                              None,                       #we_have_already_combined_libgl_to_single_package
-    'amdgpu-pro-core':                         None,                       #unneeded_meta_package
-    'amdgpu-pro-lib32':                        None,                       #we_have_already_combined_libgl_to_single_package
-    'amf-amdgpu-pro':                          'amf-amdgpu-pro',           #mapped_manually
-    'clinfo-amdgpu-pro':                       None,                       #unneeded_pro_component
-    'clinfo-amdgpu-pro:i386':                  None,                       #unneeded_pro_component
-    'libamdenc-amdgpu-pro':                    'amf-amdgpu-pro',           #mapped_manually.Maybe_this_should_go_to_a_saparate_package?
-    'libegl1-amdgpu-pro':                      'amdgpu-pro-libgl',         #mapped_manually
-    'libegl1-amdgpu-pro:i386':                 'lib32-amdgpu-pro-libgl',   #mapped_manually
-    'libgl1-amdgpu-pro-appprofiles':           'amdgpu-pro-libgl',         #mapped_manually
-    'libgl1-amdgpu-pro-dri':                   'amdgpu-pro-libgl',         #mapped_manually
-    'libgl1-amdgpu-pro-dri:i386':              'lib32-amdgpu-pro-libgl',   #mapped_manually
-    'libgl1-amdgpu-pro-ext':                   'amdgpu-pro-libgl',         #mapped_manually
-    'libgl1-amdgpu-pro-ext:i386':              'lib32-amdgpu-pro-libgl',   #mapped_manually
-    'libgl1-amdgpu-pro-glx':                   'amdgpu-pro-libgl',         #mapped_manually
-    'libgl1-amdgpu-pro-glx:i386':              'lib32-amdgpu-pro-libgl',   #mapped_manually
-    'libglapi1-amdgpu-pro':                    'amdgpu-pro-libgl',         #mapped_manually
-    'libglapi1-amdgpu-pro:i386':               'lib32-amdgpu-pro-libgl',   #mapped_manually
-    'libgles2-amdgpu-pro':                     'amdgpu-pro-libgl',         #mapped_manually
-    'libgles2-amdgpu-pro:i386':                'lib32-amdgpu-pro-libgl',   #mapped_manually
-    'ocl-icd-libopencl1-amdgpu-pro':           None,                       #unneeded_pro_component
-    'ocl-icd-libopencl1-amdgpu-pro:i386':      None,                       #unneeded_pro_component
-    'ocl-icd-libopencl1-amdgpu-pro-dev':       None,                       #unneeded_pro_component
-    'ocl-icd-libopencl1-amdgpu-pro-dev:i386':  None,                       #unneeded_pro_component
-    'opencl-legacy-amdgpu-pro-icd':            None,                       #opencl_goes_to_opencl-amd
-    'opencl-legacy-amdgpu-pro-icd:i386':       None,                       #opencl_goes_to_opencl-amd
-    'vulkan-amdgpu-pro':                       'vulkan-amdgpu-pro',        #mapped_manually
-    'vulkan-amdgpu-pro:i386':                  'lib32-vulkan-amdgpu-pro',  #mapped_manually
+    'amdgpu-pro':                              None,                                #we_have_already_combined_libgl_to_single_package
+    'amdgpu-pro-core':                         None,                                #unneeded_meta_package
+    'amdgpu-pro-lib32':                        None,                                #we_have_already_combined_libgl_to_single_package
+    'amdgpu-pro-oglp':                         'amdgpu-pro-oglp',                   #
+    'amdgpu-pro-oglp:i386':                    'lib32-amdgpu-pro-oglp',             #
+    'amf-amdgpu-pro':                          'amf-amdgpu-pro',                    #mapped_manually
+    'clinfo-amdgpu-pro':                       None,                                #unneeded_pro_component
+    'clinfo-amdgpu-pro:i386':                  None,                                #unneeded_pro_component
+    'libamdenc-amdgpu-pro':                    'amf-amdgpu-pro',                    #mapped_manually.Maybe_this_should_go_to_a_saparate_package?
+    'libegl1-amdgpu-pro-oglp':                 'libegl1-amdgpu-pro-oglp',           #
+    'libegl1-amdgpu-pro-oglp:i386':            'lib32-libegl1-amdgpu-pro-oglp',     #
+    'libgl1-amdgpu-pro-oglp-dri':              'libgl1-amdgpu-pro-oglp-dri',        #
+    'libgl1-amdgpu-pro-oglp-dri:i386':         'lib32-libgl1-amdgpu-pro-oglp-dri',  #
+    'libgl1-amdgpu-pro-oglp-ext':              'libgl1-amdgpu-pro-oglp-ext',        #
+    'libgl1-amdgpu-pro-oglp-gbm':              'libgl1-amdgpu-pro-oglp-gbm',        #
+    'libgl1-amdgpu-pro-oglp-glx':              'libgl1-amdgpu-pro-oglp-glx',        #
+    'libgl1-amdgpu-pro-oglp-glx:i386':         'lib32-libgl1-amdgpu-pro-oglp-glx',  #
+    'libgles1-amdgpu-pro-oglp':                'libgles1-amdgpu-pro-oglp',          #
+    'libgles1-amdgpu-pro-oglp:i386':           'lib32-libgles1-amdgpu-pro-oglp',    #
+    'libgles2-amdgpu-pro-oglp':                'libgles2-amdgpu-pro-oglp',          #
+    'libgles2-amdgpu-pro-oglp:i386':           'lib32-libgles2-amdgpu-pro-oglp',    #
+    'ocl-icd-libopencl1-amdgpu-pro':           None,                                #unneeded_pro_component
+    'ocl-icd-libopencl1-amdgpu-pro:i386':      None,                                #unneeded_pro_component
+    'ocl-icd-libopencl1-amdgpu-pro-dev':       None,                                #unneeded_pro_component
+    'ocl-icd-libopencl1-amdgpu-pro-dev:i386':  None,                                #unneeded_pro_component
+    'opencl-legacy-amdgpu-pro-icd':            None,                                #opencl_goes_to_opencl-amd
+    'opencl-legacy-amdgpu-pro-icd:i386':       None,                                #opencl_goes_to_opencl-amd
+    'vulkan-amdgpu-pro':                       'vulkan-amdgpu-pro',                 #mapped_manually
+    'vulkan-amdgpu-pro:i386':                  'lib32-vulkan-amdgpu-pro',           #mapped_manually
 }
--- replace_deps.py
+++ replace_deps.py
@@ -5,23 +5,17 @@ replace_deps = {
     'amdgpu-core':                'amdgpu-core',          #could_not_auto_translate
     'amdgpu-lib32':               'amdgpu-lib32',         #could_not_auto_translate
     'libc6':                      None,                   #manually_mapped
-    'libdrm2':                    'libdrm',               #auto_translated
     'libdrm2-amdgpu':             'libdrm',               #auto_translated
     'libdrm-amdgpu-amdgpu1':      'libdrm',               #auto_translated
+    'libgbm1-amdgpu':             'gbm',                  #auto_translated
     'libgcc-s1':                  None,                   #manually_mapped
-    'libssl1.1':                  'openssl',              #auto_translated
+    'libgl1':                     'libglvnd',             #manually_mapped
+    'libssl1.1':                  'openssl-1.1',          #auto_translated
     'libstdc++6':                 None,                   #manually_mapped
     'libvulkan1':                 'vulkan-icd-loader',    #auto_translated
     'libwayland-amdgpu-client0':  'wayland',              #auto_translated
-    'libwayland-amdgpu-server0':  'wayland',              #auto_translated
+    'libwayland-client0':         'wayland',              #auto_translated
     'libx11-6':                   'libx11',               #auto_translated
-    'libx11-xcb1':                'libx11',               #auto_translated
-    'libxcb1':                    'libxcb',               #auto_translated
-    'libxcb-dri2-0':              'libxcb',               #auto_translated
-    'libxcb-glx0':                'libxcb',               #auto_translated
-    'libxdamage1':                'libxdamage',           #auto_translated
-    'libxext6':                   'libxext',              #auto_translated
-    'libxfixes3':                 'libxfixes',            #auto_translated
-    'libxxf86vm1':                'libxxf86vm',           #auto_translated
     'rocm-opencl-runtime':        'rocm-opencl-runtime',  #auto_translated
+    'zlib1g':                     'zlib',                 #auto_translated
 }
