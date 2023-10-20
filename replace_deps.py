b19d8bfbc48c0dd9841be42b806853947fec94cb
mesmer <ldepaulaf@gmail.com>
2023-10-20 18:48:00 -0300

    add version 5.7.1 firmware 1.28

--- replace_deps.py
+++ replace_deps.py
@@ -5,8 +5,8 @@ replace_deps = {
     'amdgpu-core':                'amdgpu-core',          #could_not_auto_translate
     'amdgpu-lib32':               'amdgpu-lib32',         #could_not_auto_translate
     'libc6':                      None,                   #manually_mapped
-    'libdrm2-amdgpu':             'libdrm',               #auto_translated
     'libdrm-amdgpu-amdgpu1':      'libdrm',               #auto_translated
+    'libdrm2-amdgpu':             'libdrm',               #auto_translated
     'libgbm1-amdgpu':             None,                   #manually_Do_not_know_what_it_is
     'libgcc-s1':                  None,                   #manually_mapped
     'libgl1':                     'libglvnd',             #manually_mapped
@@ -17,5 +17,5 @@ replace_deps = {
     'libwayland-client0':         'wayland',              #auto_translated
     'libx11-6':                   'libx11',               #auto_translated
     'rocm-opencl-runtime':        'rocm-opencl-runtime',  #manually_mapped
-    'zlib1g':                     'zlib',                 #auto_translated
+    'zlib1g':                     'zlib',                 #manually_mapped
 }
