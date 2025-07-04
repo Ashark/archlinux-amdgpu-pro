1c98c2b4e20aec403202d600868e20eae6dc1d0e
mesmer <ldepaulaf@gmail.com>
2025-07-04 00:31:23 -0300

    update version

--- translate_deb_to_arch_dependency.sh
+++ translate_deb_to_arch_dependency.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
+set -euo pipefail
 
-# This script takes a name of debian package name and outputs suggested arch linux package name
+# This script takes a Debian package name and outputs suggested Arch Linux package name
 
-# Making it in individual folder to support multi processing
-tmp_dir=$(mktemp -d -t deb_to_arch_${line}-$(date +%Y-%m-%d-%H-%M-%S)-XXX)
-cd $tmp_dir
+pkg_name="$1"
 
-rm -rf tmp-mapdeps-pkg
+tmp_dir=$(mktemp -d -t deb_to_arch_${pkg_name}-$(date +%Y-%m-%d-%H-%M-%S)-XXX)
+cd "$tmp_dir"
+
 mkdir -p tmp-mapdeps-pkg/DEBIAN
-cat << EOF  > tmp-mapdeps-pkg/DEBIAN/control
+cat << EOF > tmp-mapdeps-pkg/DEBIAN/control
 Package: tmp-mapdeps-pkg
 Version: 1.0-1
 Section: base
 Priority: optional
 Architecture: amd64
-Depends: $1
+Depends: $pkg_name
 Maintainer: Your Name <you@email.com>
 Description: Temporary package to workaround https://github.com/helixarch/debtap/issues/41
 EOF
@@ -22,16 +23,32 @@ EOF
 dpkg-deb --build tmp-mapdeps-pkg > /dev/null
 
 debtap -P -Q tmp-mapdeps-pkg.deb 2>/dev/null > tmpfile
-if grep "Warning: These dependencies" tmpfile > /dev/null; then
+
+if grep -q "Warning: These dependencies" tmpfile; then
     echo could_not_translate
     rm tmp-mapdeps-pkg.deb tmpfile
     rm -rf tmp-mapdeps-pkg
+    cd ..
+    rm -rf "$tmp_dir"
     exit 1
 fi
 
-cat tmp-mapdeps-pkg/PKGBUILD | grep depends | sed 's/depends=('\''//' | sed 's/'\'' '\''/\n/g' | sed 's/'\'')//g'
-rm tmp-mapdeps-pkg.deb tmpfile
-rm -rf tmp-mapdeps-pkg 
+if [ ! -f tmp-mapdeps-pkg/PKGBUILD ]; then
+    echo could_not_translate
+    rm tmp-mapdeps-pkg.deb tmpfile
+    rm -rf tmp-mapdeps-pkg
+    cd ..
+    rm -rf "$tmp_dir"
+    exit 1
+fi
 
+grep depends tmp-mapdeps-pkg/PKGBUILD \
+    | sed "s/depends=('//" \
+    | sed "s/' '/\n/g" \
+    | sed "s/')//g"
+
+rm tmp-mapdeps-pkg.deb tmpfile
+rm -rf tmp-mapdeps-pkg
 cd ..
-rm -rf $tmp_dir
+rm -rf "$tmp_dir"
+
